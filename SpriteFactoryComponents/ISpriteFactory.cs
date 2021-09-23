d93e1e6acc555ce2feb2d5654c19177826b0efe4
Chaeun Hong <hong.930@osu.edu>
2021-09-22 20:27:08 -0400

    Recreate and Rename SpriteFactory part.

---
+++ SpriteFactoryComponents/ISpriteFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.SpriteFactoryComponents
+{
+    public interface ISpriteFactory
+    {
+        public static ISpriteFactory Instance { get; }
+        void LoadAllTextures(ContentManager content);
+    }
+}
