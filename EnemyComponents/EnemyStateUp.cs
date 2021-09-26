525927695f63064383e17c780a1134180c57e75a
Andy Kroh <90639840+ajkroh@users.noreply.github.com>
2021-09-26 14:27:35 -0400

    Add files via upload

---
+++ EnemyComponents/EnemyStateUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Project1.Content.EnemyComponents;
+
+namespace Project1.EnemyComponents
+{
+    class EnemyStateUp : IEnemyDirectionState
+    {
+        public IEnemy Enemy { get; set; }
+        public string ID { get; set; }
+        public EnemyStateUp(IEnemy enemy)
+        {
+            Enemy = enemy;
+            ID = "Up";
+        }
+
+        public void MoveDown()
+        {
+            Enemy.EnemyDirectionState = new EnemyStateDown(Enemy);
+        }
+
+        public void MoveLeft()
+        {
+            Enemy.EnemyDirectionState = new EnemyStateLeft(Enemy);
+        }
+
+        public void MoveRight()
+        {
+            Enemy.EnemyDirectionState = new EnemyStateRight(Enemy);
+        }
+
+        public void MoveUp()
+        {
+
+        }
+    }
+}
