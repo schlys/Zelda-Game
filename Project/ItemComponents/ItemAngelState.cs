4146a90a6990558649029d70c8cdc2d1959fba6b
Elise Kosmides <kosmides.3@buckeyemail.osu.edu>
2021-10-01 17:13:23 -0400

    make angel item move in random directions

--- Project/ItemComponents/ItemAngelState.cs
+++ Project/ItemComponents/ItemAngelState.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using Project1.SpriteFactoryComponents;
+using System; 
 
 namespace Project1.ItemComponents
 {
@@ -9,6 +10,11 @@ namespace Project1.ItemComponents
     {
         public IItem Item { get; set; }
         public Sprite Sprite { get; set; }
+        private Random R = new Random();
+        private int Timer = 0;
+        private int Rand;
+        private int Step = 1; 
+
         public ItemAngelState(IItem item)
         {
             Item = item;
@@ -20,7 +26,49 @@ namespace Project1.ItemComponents
         }
         public void Update()
         {
+            // Switch fairy direction every 15 calls 
+            Timer++; 
+            if (Timer % 15 == 0)
+            {
+                Rand = R.Next(4);
+            }
+
+            switch (Rand)
+            {
+                case 0:
+                    MoveUp();
+                    break;
+                case 1:
+                    MoveDown();
+                    break;
+                case 2:
+                    MoveLeft();
+                    break;
+                case 3:
+                    MoveRight();
+                    break;
+                default:
+                    MoveUp();
+                    break;
+            }
+
             Sprite.Update();
+        }
+        public void MoveUp()
+        {
+            Item.Position = new Vector2(Item.Position.X, Item.Position.Y - Step); 
+        }
+        public void MoveDown()
+        {
+            Item.Position = new Vector2(Item.Position.X, Item.Position.Y + Step);
+        }
+        public void MoveLeft()
+        {
+            Item.Position = new Vector2(Item.Position.X - Step, Item.Position.Y);
+        }
+        public void MoveRight()
+        {
+            Item.Position = new Vector2(Item.Position.X + Step, Item.Position.Y);
         }
     }
 }
