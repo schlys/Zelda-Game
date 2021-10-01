5f0ab9383e16a4bdc39f066e51fd4a0f04313d89
Chaeun Hong <hong.930@osu.edu>
2021-10-01 19:42:05 -0400

    Make Moblin shoot the arrow.

--- Project/EnemyComponents/EnemyStateMoblin.cs
+++ Project/EnemyComponents/EnemyStateMoblin.cs
@@ -12,10 +12,18 @@ namespace Project1.EnemyComponents
         public IEnemyDirectionState DirectionState { get; set; }
         public Sprite Sprite { get; set; }     
         public string ID { get; set; }
+        private bool isAttacking;
         private int step;
-        private int movementTimer;
+        private int movementTimer=0;
+        private int poofTimer = 0;
         private Random r = new Random();
         private int randomInt;
+
+        private IAquamentusProjectile up = new NoAquamentusProjectile();
+        private IAquamentusProjectile down = new NoAquamentusProjectile();
+        private IAquamentusProjectile right = new NoAquamentusProjectile();
+        private IAquamentusProjectile left = new NoAquamentusProjectile();
+
         public EnemyStateMoblin(IEnemy enemy)
         {
             Enemy = enemy;
@@ -24,6 +32,7 @@ namespace Project1.EnemyComponents
             UpdateSprite();
             randomInt = r.Next(0, 5);
             step = 1;
+            isAttacking = false;
         }
 
         private void MoveUp()
@@ -70,17 +79,56 @@ namespace Project1.EnemyComponents
         {
             Sprite = SpriteFactory.Instance.GetSpriteData(ID + DirectionState.ID);
         }
+
+        public void Attack(string direction)
+        {
+            if (!isAttacking)
+            {
+                isAttacking = true;
+                Sprite.MaxDelay = 30;
+                if(direction.Equals("Up"))
+                    up = new MoblinProjectile(Enemy.Position, "Up");
+                else if(direction.Equals("Down"))
+                    down = new MoblinProjectile(Enemy.Position, "Down");
+                else if(direction.Equals("Right"))
+                    right = new MoblinProjectile(Enemy.Position, "Right");
+                else if(direction.Equals("Left"))
+                    left = new MoblinProjectile(Enemy.Position, "Left");
+            }
+        }
+
         public void Draw(SpriteBatch spriteBatch, Vector2 position)
         {
             Sprite.Draw(spriteBatch, position, 80);     // TODO: not hardcode 80 
+            up.Draw(spriteBatch);
+            down.Draw(spriteBatch);
+            right.Draw(spriteBatch);
+            left.Draw(spriteBatch);
         }
 
         public void Update()
         {
             Sprite.Update();
+            up.Update();
+            down.Update();
+            right.Update();
+            left.Update();
+
             movementTimer++;
+
+            if (Sprite.CurrentFrame == Sprite.TotalFrames)
+            {
+
+                if (isAttacking)
+                {
+                    isAttacking = false;
+                }
+            }
+
             if (movementTimer > 90)
             {
+                if(Sprite.CurrentFrame==1) //Moblin shoot the arrow when it stops
+                    Attack(DirectionState.ID);
                 randomInt = r.Next(0, 5);
                 movementTimer = 0;
             }
---
+++ Project/EnemyComponents/MoblinProjectile.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Project1.SpriteFactoryComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.EnemyComponents
+{
+    class MoblinProjectile : IAquamentusProjectile
+    {
+        Sprite Sprite;
+        public string ID;
+        private Vector2 position;
+        private string direction;
+        private int counter;
+        public MoblinProjectile(Vector2 position, string direction)
+        {
+            this.direction = direction;
+            this.position = position;
+            counter = 0;
+            ID = "MoblinProjectile";
+            Sprite = SpriteFactory.Instance.GetSpriteData(ID+ direction);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (counter < 200)
+                Sprite.Draw(spriteBatch, position, 80);
+        }
+        public void Update()
+        {
+            Sprite.Update();
+            counter++;
+            if (counter < 200)
+            {
+                if (direction.Equals("Up"))
+                    position += new Vector2(0, (float) -2);
+                else if (direction.Equals("Down"))
+                    position += new Vector2(0, (float)2);
+                else if (direction.Equals("Right"))
+                    position += new Vector2((float)2, 0);
+                else if (direction.Equals("Left"))
+                    position += new Vector2((float)-2, 0);
+            }
+        }
+    }
+}
