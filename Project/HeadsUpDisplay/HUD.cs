d3e05d0c03f9a195bbfb8e4baa95b287fddf7211
Elise Kosmides <kosmides.3@buckeyemail.osu.edu>
2021-11-02 16:25:55 -0400

    draw link health hearts for HUD

--- Project/HeadsUpDisplay/HUD.cs
+++ Project/HeadsUpDisplay/HUD.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Project1.LevelComponents;
+using Project1.SpriteComponents; 
 
 namespace Project1.HeadsUpDisplay
 {
@@ -36,6 +37,34 @@ namespace Project1.HeadsUpDisplay
 
             // Draw the <LevelMap> found in <LevelFactory>
             LevelFactory.Instance.LevelMap.Draw(spriteBatch);
+
+            // Draw Link's Health Hearts 
+            DrawLinkHealth(spriteBatch); 
+        }
+
+        private void DrawLinkHealth(SpriteBatch spriteBatch)
+        {
+            Sprite HeartFull = SpriteFactory.Instance.GetSpriteData("HeartFull");
+            Sprite HeartHalf = SpriteFactory.Instance.GetSpriteData("HeartHalf");
+            Sprite HeartEmpty = SpriteFactory.Instance.GetSpriteData("HeartEmpty");
+
+            Vector2 position = new Vector2(200, 10);
+            int spaceX = (int)(HeartFull.HitBox.X * GameObjectManager.Instance.ScalingFactor * 1.5); 
+            LinkHealth Health = Link.Health;
+            for(int i = 1; i <= Health.TotalNumHearts; i++)
+            {
+                if(Health.CurrNumHearts >= i)
+                {
+                    HeartFull.Draw(spriteBatch, position); 
+                } else if (i - Health.CurrNumHearts < 1)
+                {
+                    HeartHalf.Draw(spriteBatch, position);
+                } else
+                {
+                    HeartEmpty.Draw(spriteBatch, position);
+                }
+                position.X += spaceX;
+            }
         }
         public void Reset()
         {
