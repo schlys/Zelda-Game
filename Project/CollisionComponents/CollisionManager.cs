2bef7e9d385e488f05eaccfcc46d8882addb4c80
Elise Kosmides <kosmides.3@buckeyemail.osu.edu>
2021-11-18 20:51:25 -0500

    Refactor: collisionManager to make a method for duplicate code

--- Project/CollisionComponents/CollisionManager.cs
+++ Project/CollisionComponents/CollisionManager.cs
@@ -75,6 +75,7 @@ namespace Project1.CollisionComponents
         {
             ICollidable item1 = collision.Item1;
             ICollidable item2 = collision.Item2;
+            
             // Get the type of commands to execute from command mappings
             ConstructorInfo constructor1 = commands.Item1;
             ConstructorInfo constructor2 = commands.Item2;
@@ -115,13 +116,13 @@ namespace Project1.CollisionComponents
             }
         }
 
-        // NOTE: Returns the proper hitbox given the position and hitbox dimensions
-        public Rectangle GetHitBox(Vector2 position, Vector2 dimensions, int size)
+        // NOTE: Returns the proper hitbox given the position, hitbox dimensions, and the size of the original rectangle
+        /*public Rectangle GetHitBox(Vector2 position, Vector2 dimensions, int size)
         {
             int xPos = (int)(position.X + (size / 2) - (dimensions.X / 2));
             int yPos = (int)(position.Y + (size / 2) - (dimensions.Y / 2));
             return new Rectangle(xPos, yPos, (int)dimensions.X, (int)dimensions.Y); 
-        }
+        }*/
 
         // NOTE: Returns the proper hitbox given the position and hitbox dimensions
         public Rectangle GetHitBox(Vector2 position, Vector2 dimensions)
@@ -136,8 +137,8 @@ namespace Project1.CollisionComponents
 
         public void DetectCollisions()
         {
-            // TODO: try to make iteration more efficent 
             // NOTE: Compare all moving objects to all other non-moving and moving objects 
+
             for (int i = 0; i < MovingObjects.Count; i++)
             {
                 ICollidable item1 = MovingObjects[i];
@@ -145,35 +146,32 @@ namespace Project1.CollisionComponents
                 for (int j = i+1; j < MovingObjects.Count; j++)
                 {
                     ICollidable item2 = MovingObjects[j];
-                    ICollision collision = DetectCollision(item1, item2); 
-                    if (!collision.GetType().Name.ToString().Equals("NullCollision"))
-                    {
-                        Tuple<ConstructorInfo, ConstructorInfo> commands = GetCommands(collision); 
-                        if(commands != null)
-                        {
-                            ExecuteCommands(collision, commands); 
-                        }
-                    }
+                    Collide(item1, item2); 
                 }
 
                 foreach (ICollidable item2 in NonMovingObjects)
                 {
-                   
-                    ICollision collision = DetectCollision(item1, item2);
-                    if (!collision.GetType().Name.ToString().Equals("NullCollision"))
-                    {
-                        Tuple<ConstructorInfo, ConstructorInfo> commands = GetCommands(collision);
-                        if (commands != null)
-                        {
-                            ExecuteCommands(collision, commands);
-                        }
-                        // break here so movers don't get knocked back by two different blocks
-                        //break;
-                    }
-  
+                    Collide(item1, item2);
                 }
             }
         }
+        private void Collide(ICollidable item1, ICollidable item2)
+        {
+            /* Detech if there is a collision between <Item1> and <Item2>, if so execute their 
+             * collision if one exists. 
+             */
+
+            ICollision collision = DetectCollision(item1, item2);
+            if (!collision.GetType().Name.ToString().Equals("NullCollision"))
+            {
+                Tuple<ConstructorInfo, ConstructorInfo> commands = GetCommands(collision);
+                if (commands != null)
+                {
+                    ExecuteCommands(collision, commands);
+                }
+            }
+        }
+
         public ICollision DetectCollision(ICollidable item1, ICollidable item2)
         {
             /* NOTE: Finds the direction of the collision based on the minimum intersection dimension
