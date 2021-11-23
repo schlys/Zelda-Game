2cedf033027a25b8723bda6f8cbe0e7d90d40f5a
schlys <samchlystek20@gmail.com>
2021-11-23 17:50:20 -0500

    add fire to old man room

---
+++ Project/ItemComponents/ItemFireState.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Project1.LinkComponents;
+using Project1.SpriteComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.ItemComponents
+{
+    class ItemFireState : IItemState
+    {
+        public IItem Item { get; set; }
+        public Sprite Sprite { get; set; }
+        public bool IsMoving { get; set; }
+        public string ID { get; set; }
+       
+
+        public ItemFireState(IItem item)
+        {
+            Item = item;
+            IsMoving = false;
+            ID = item.Kind;
+            Sprite = SpriteFactory.Instance.GetSpriteData(item.Kind);
+        }
+
+        public void AddToInventory(ILink link)
+        {
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Sprite.Draw(spriteBatch, Item.Position);
+        }
+
+        public void Update()
+        {
+            Sprite.Update();
+        }
+
+        public void UseItem(ILink link)
+        {
+            
+        }
+    }
+}
+
