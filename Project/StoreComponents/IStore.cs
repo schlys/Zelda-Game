ff72127cd82095be17c10ff5b1408f53ad40ea56
Elise Kosmides <kosmides.3@buckeyemail.osu.edu>
2021-11-28 20:29:28 -0500

    create storecomponents folder, store, and istore for old man's store

---
+++ Project/StoreComponents/IStore.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Project1.LinkComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.StoreComponents
+{
+    public interface IStore
+    {
+        ILink Link { get; set; }
+        void PurchaseItem1();
+        void PurchaseItem2();
+        void PurchaseItem3();
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+        void Reset();
+        
+    }
+}
---
+++ Project/StoreComponents/Store.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Project1.LinkComponents;
+using Project1.ItemComponents;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project1.StoreComponents
+{
+    public class Store : IStore
+    {
+        public ILink Link { get; set; }
+
+        private Game1 Game;
+        private SpriteFont Font;
+
+        private IItem Item1;
+        private IItem Item2;
+        private IItem Item3; 
+        private int PriceItem1;
+        private int PriceItem2;
+        private int PriceItem3;
+
+
+        public Store(ILink link, Game1 game)
+        {
+            Link = link;
+            Game = game;
+
+            Font = Game.Content.Load<SpriteFont>(GameVar.Font);
+
+        }
+
+        public void PurchaseItem1()
+        {
+
+        }
+        public void PurchaseItem2()
+        {
+
+        }
+        public void PurchaseItem3()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+        public void Reset()
+        {
+
+        }
+    }
+}
