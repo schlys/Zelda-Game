c75e568c54495e6f2fa75d62fe307a522cb0872b
Elise Kosmides <kosmides.3@buckeyemail.osu.edu>
2021-12-01 19:00:16 -0500

    Refactor Collision Components

--- Project/CollisionComponents/CollisionManager.cs
+++ Project/CollisionComponents/CollisionManager.cs
@@ -20,9 +20,9 @@ namespace Project1.CollisionComponents
             }
         }
 
-        public List<ICollidable> MovingObjects;
+        private List<ICollidable> MovingObjects;
 
-        public List<ICollidable> NonMovingObjects;
+        private List<ICollidable> NonMovingObjects;
 
         private static Dictionary<string, Tuple<ConstructorInfo, ConstructorInfo>> CollisionMappings;
 
@@ -35,9 +35,12 @@ namespace Project1.CollisionComponents
 
         private void CreateDict()
         {
+            /* Initalizes and loads <CollisionMappings> form the file <XMLCollisions.xml> with collisions 
+             * and their corresponding commands. 
+             */
+            
             CollisionMappings = new Dictionary<string, Tuple<ConstructorInfo, ConstructorInfo>>();
 
-            // NOTE: Load the commands for each collision from an XML document into the CollisionMappings dictionary 
             XmlDocument XMLData = new XmlDocument();
             var path = AppDomain.CurrentDomain.BaseDirectory + "XMLData/XMLCollisions.xml";
             XMLData.Load(path);
@@ -62,17 +65,32 @@ namespace Project1.CollisionComponents
                 CollisionMappings.Add(name, Tuple.Create(constructor1, constructor2));
             }   
         }
+
         public Tuple<ConstructorInfo, ConstructorInfo> GetCommands(ICollision collision)
         {
-            string key = collision.DirectionKey;
-            if (CollisionMappings.ContainsKey(key)) return CollisionMappings[key];
-            else if (CollisionMappings.ContainsKey(collision.Key)) return CollisionMappings[collision.Key];
+            /* Return the two commands for the corresponding <collision> found in <CollisionMappings>. 
+             * The key may be the <DirectionKey> property for direction specific collisions or the <Key> 
+             * property for non direction specific collisions. 
+             */ 
+
+            if (CollisionMappings.ContainsKey(collision.DirectionKey))
+            {
+                return CollisionMappings[collision.DirectionKey];
+            }
+            else if (CollisionMappings.ContainsKey(collision.Key))
+            {
+                return CollisionMappings[collision.Key];
+            }
 
             return null; 
         }
 
         public void ExecuteCommands(ICollision collision, Tuple<ConstructorInfo, ConstructorInfo> commands)
         {
+            /* Given the <collision> and two corresponding <commands>, execture the commands on the appropriate 
+             * items of <collision> if they are not NullCollision. 
+             */ 
+            
             ICollidable item1 = collision.Item1;
             ICollidable item2 = collision.Item2;
             
@@ -84,7 +102,7 @@ namespace Project1.CollisionComponents
             object command1 = constructor1.Invoke(new object[] { item1, item2, collision.Direction });
             object command2 = constructor2.Invoke(new object[] { item2, item1, collision.Direction });
 
-            // Execute the commands if they are not "NoCmd" 
+            // Execute the commands if they are not NullCollision  
             ICommand cmd1 = (ICommand)command1;
             ICommand cmd2 = (ICommand)command2;
 
@@ -115,20 +133,28 @@ namespace Project1.CollisionComponents
             }
         }
 
-        // NOTE: Returns the proper hitbox given the position and hitbox dimensions
         public Rectangle GetHitBox(Vector2 position, Vector2 dimensions)
         {
+            /* Returns the proper hitbox given the position and hitbox dimensions. 
+             * The hitbox is centered within a square of <size>. 
+             */
+
             int size = SpriteFactory.Instance.UniversalSize * GameVar.ScalingFactor;
+            
             dimensions.X *= GameVar.ScalingFactor;
             dimensions.Y *= GameVar.ScalingFactor;
+            
             int xPos = (int)(position.X + (size / 2) - (dimensions.X / 2));
             int yPos = (int)(position.Y + (size / 2) - (dimensions.Y / 2));
+            
             return new Rectangle(xPos, yPos, (int)dimensions.X, (int)dimensions.Y);
         }
 
         public void DetectCollisions()
         {
-            // NOTE: Compare all moving objects to all other non-moving and moving objects 
+            /* Compare all moving objects to all other non-moving and moving objects. 
+             */ 
+            
             for (int i = 0; i < MovingObjects.Count; i++)
             {
                 ICollidable item1 = MovingObjects[i];
@@ -147,8 +173,8 @@ namespace Project1.CollisionComponents
         }
         private void Collide(ICollidable item1, ICollidable item2)
         {
-            /* Detech if there is a collision between <Item1> and <Item2>, if so execute their 
-             * collision if one exists. 
+            /* Detect if there is a collision between <Item1> and <Item2>, if so execute their 
+             * collision. 
              */
 
             ICollision collision = DetectCollision(item1, item2);
